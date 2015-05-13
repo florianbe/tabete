284adcd47ab29831ed906d35d7ea9576ee902572
Florian Binoeder <florian.binoeder@gmail.com>
2015-05-13 17:07:12 +0200

    Base Layout

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', [])
+angular.module('tabete.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
   // Form data for the login modal
